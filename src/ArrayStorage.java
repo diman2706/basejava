eff3f97b6b20c40a7b31cd3b07618e1a85f9c013
diman2706 <59848064+diman2706@users.noreply.github.com>
2020-01-23 03:59:57 +0300

    Первое ДЗ Петрова Дмитрия

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,60 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int i = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        i = 0;
     }
 
     void save(Resume r) {
+        if (storage[i] == null){
+            storage[i] = r;
+            i++;
+        }else storage[size()] =r;
+
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            resume.uuid = uuid;
+            return resume;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = 0;
+        for (int j = 0; j <size() ; j++) {
+           if (storage[j].uuid.equals(uuid)){
+               index = j;
+               Resume[] resultStorage = new Resume[storage.length-1];
+               System.arraycopy(storage,0,resultStorage,0,index);
+               System.arraycopy(storage,index+1,resultStorage,index,storage.length-index-1);
+               storage = resultStorage;
+               i = index;
+           }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage,size());
     }
 
     int size() {
-        return 0;
+        int v = 0;
+        for (int j = 0; j <storage.length ; j++) {
+            if (!(storage[j] == null)) v++;
+        }
+        return v;
     }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -22,7 +22,9 @@ public class MainTestArrayStorage {
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
+
         ARRAY_STORAGE.delete(r1.uuid);
+
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
