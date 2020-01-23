a204cb2dbc12810f6868d1e29947f7a34b3866f9
Дмитрий <59848064+diman2706@users.noreply.github.com>
2020-01-23 22:10:32 +0300

    внес исправления

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,60 +1,49 @@
 import java.util.Arrays;
 
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int i = 0;
+    int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
-        i = 0;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
-        if (storage[i] == null){
-            storage[i] = r;
-            i++;
-        }else storage[size()] =r;
-
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (Resume resume : storage) {
-            resume.uuid = uuid;
-            return resume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
         int index = 0;
-        for (int j = 0; j <size() ; j++) {
-           if (storage[j].uuid.equals(uuid)){
-               index = j;
-               Resume[] resultStorage = new Resume[storage.length-1];
-               System.arraycopy(storage,0,resultStorage,0,index);
-               System.arraycopy(storage,index+1,resultStorage,index,storage.length-index-1);
-               storage = resultStorage;
-               i = index;
-           }
+        for (int j = 0; j < size; j++) {
+            if (storage[j].uuid.equals(uuid)) {
+                index = j;
+                System.arraycopy(storage, 0, storage, 0, index);
+                System.arraycopy(storage, index + 1, storage, index, storage.length - index - 1);
+                size--;
+            }
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage,size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int v = 0;
-        for (int j = 0; j <storage.length ; j++) {
-            if (!(storage[j] == null)) v++;
-        }
-        return v;
+        return size;
     }
 }
+
