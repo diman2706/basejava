a513118ce116ff30b52b92b3d706f6862c36fd80
Дмитрий <59848064+diman2706@users.noreply.github.com>
2020-01-28 21:59:53 +0300

    Переделал метод isResumeContainsofStorage.

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -17,9 +17,11 @@ public class MainTestArrayStorage {
         Resume r3 = new Resume();
         r3.setUuid("uuid3");
 
+        ARRAY_STORAGE.update(r1);
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.update(r1);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
