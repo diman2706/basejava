0aa637c77eaa7a7d31f28efc4bf752e18be3690b
Дмитрий <59848064+diman2706@users.noreply.github.com>
2020-04-27 19:01:32 +0300

    Lesson 16 make ResumeServlet logic

--- src/com/urise/webapp/model/ContactType.java
+++ src/com/urise/webapp/model/ContactType.java
@@ -2,12 +2,42 @@ package com.urise.webapp.model;
 
 public enum ContactType {
     PHONENUMBER("Тел."),
-    SKYPE("Skype"),
-    MAIL("Почта"),
-    LINCKEDLN("Профиль LinkedIn"),
-    GITHUB("Профиль GitHub"),
-    STACKOVERFLOW("Профиль Stackoverflow"),
-    HOMEPAGE("Домашняя страница");
+    SKYPE("Skype"){
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("skype:" + value, value);
+        }
+    },
+    MAIL("Почта"){
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("mailto:" + value, value);
+        }
+    },
+    LINCKEDLN("Профиль LinkedIn"){
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    GITHUB("Профиль GitHub"){
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    STACKOVERFLOW("Профиль Stackoverflow"){
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    HOMEPAGE("Домашняя страница"){
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    };
 
     private String title;
 
@@ -17,5 +47,21 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    public static String toLink(String href, String title) {
+        return "<a href='" + href + "'>" + title + "</a>";
+    }
+
+    public String toLink(String href) {
+        return toLink(href, title);
+    }
+
+    protected String toHtml0(String value) {
+        return title + ": " + value;
+    }
+
+    public String toHtml(String value) {
+        return (value == null) ? "" : toHtml0(value);
     }
 }
